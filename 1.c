fcccf24acbb9d16f35bd092b74f03c7ef2966fab
tezzvolitov <l.angelgarrido11@gmail.com>
2025-03-17 23:15:53 -0600

    hola
    codigo 1

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    printf("Este es un texto \n");
+    printf("que fue impreso desde c");
+    printf("\n");
+    printf("ya soy todo un programador");
+    printf("\n");
+    system("Pause");
+    return 0;
+}
+
