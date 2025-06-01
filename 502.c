46892bde0eac17b84f761b0286626c419344c77a
tezzvolitov <l.angelgarrido11@gmail.com>
2025-05-31 18:46:01 -0600

    codigo 502
    no se que hay

---
+++ 502.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(){
+char cadena [10];
+printf("un int ocupa %d\n byres",sizeof(int));
+printf("un char ocupa %d\n byres",sizeof(char));
+printf("un float ocupa %d\n byres",sizeof(float));
+printf("un double ocupa %d\n byres",sizeof(double));
+printf("cadena ocupa %dn byres",sizeof(cadena));
+ 
+return 0;
+}
